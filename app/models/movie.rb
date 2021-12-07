b821b9a6e3de690dfa78a8627900ce8bd1e6e150
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2021-12-07 15:57:47 +0700

    validate model movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,10 @@
 class Movie < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :movie_type, presence: true
+  validates :duration, presence: true
+  validates :date_in, presence: true
+  validate :date_out_after_date_in?
+
   has_one_attached :poster
 
   enum movie_type: {
@@ -11,4 +17,11 @@ class Movie < ApplicationRecord
     Fantasy: 6,
     SciFi: 7
   }
+
+  def date_out_after_date_in?
+    if date_out < date_in
+      errors.add :date_out, "must be after date in"
+    end
+  end
+
 end
